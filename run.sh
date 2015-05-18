309299ff139d39794f3a24c24ba523e758645202
gustavodgbernardo <gustavo.bernardo@gmail.com>
2015-05-18 00:15:14 -0300

    add data and correct run.sh
    add data and correct run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 hadoop jar /usr/hdp/2.2.0.0-2041/hadoop-mapreduce/hadoop-streaming-2.6.0.2.2.0.0-2041.jar \
 -D mapred.map.tasks=2 \
--D mapred.reduce.tasks=20 \
+-D mapred.reduce.tasks=100 \
 -D stream.map.output.field.separator=, \
 -D stream.reduce.input.field.separator=, \
--file /home/rstudio/maper.R -mapper "Rscript /home/rstudio/maper.R 20 10" \
+-file /home/rstudio/maper.R -mapper "Rscript /home/rstudio/maper.R 100 10" \
 -file /home/rstudio/reducer.R -reducer "Rscript /home/rstudio/reducer.R 0.8 1 5" \
--input /tmp/RF/* -output /tmp/rf_selection 
+-input /tmp/RF/* -output /tmp/rf_selection2 
 
+hadoop fs -cat /tmp/rf_selection2/* | Rscript /home/rstudio/aggregate.R > final_selection.csv
